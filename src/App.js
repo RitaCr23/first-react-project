37c97a5c0a27fbaf849da96b94c1ac1cc2a0a1cd
Rita <96919690+RitaCr23@users.noreply.github.com>
2022-02-18 17:20:57 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from "react";
+import Info from "../src/components/Info";
+import About from "../src/components/About";
+import Interests from "../src/components/Interests";
+import Footer from "../src/components/Footer";
 
+export default function App() {
+  return (
+    <div className="container">
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </div>
+  );
+}
